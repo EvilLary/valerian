52b0bf44a8cf4619a5d644e0ca855089d721fc24
EvilLary <EvilLary@proton.me>
2025-01-23 02:38:49 +0300

    Detch Clap,Reqwest & Image

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Valerian
 simple program to fetch cars from [TheCatAPI](https://thecatapi.com/)
 
+It's a [ccat copycat](https://github.com/plastic-bottleneck/ccat) but way slower and more bloated :)
+
 ![hola](assets/hola.jpg)
 
-It's a [ccat copycat](https://github.com/plastic-bottleneck/ccat)
 ## Installation
 ```bash
 git clone https://codeberg.org/EvilLary/Valerian.git
@@ -11,7 +12,12 @@ cd Valerian
 cargo build --release
 cp ./target/release/valerian $HOME/.local/bin/
 ```
-## Usage 
+## Usage
+
 ```bash
-valerian --count 3 --output $HOME/Downloads/
+valerian number-of-cars output-directory
+```
+
+```bash
+valerian 3 $HOME/Downloads/
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,100 @@
 //All credit goes to https://github.com/plastic-bottleneck/ccat
 //This is just for learning rust
-use clap::Parser;
+
+use curl::easy::Easy;
 use serde::Deserialize;
 use std::{ffi::OsStr, path::Path};
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// output directory
-    #[arg(short, long, default_value_t = String::from(".") )]
-    output: String,
-
-    /// Number of cars to download
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
-
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize)]
 struct CarResponse {
     id: String,
     url: String,
-    //width: u16,
-    //height: u16,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    //no need to reference count
-    let response: Vec<CarResponse> = get_cars(args.count)?;
-    download_cars(&response, &args.output)?;
+fn main() -> Result<(), curl::Error> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    let count: u8 = match args.get(0) {
+        Some(c) => {
+            match c.parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    eprintln!("first command isn't number defaulting to 1");
+                    1
+                }
+            }
+        }
+        None => 1,
+    };
+    let output: String = match args.get(1) {
+        Some(o) => String::from(o),
+        None => String::from("."),
+    };
+
+    let response: Vec<CarResponse> = get_cars(count)?;
+    download_cars(&response, &output)?;
     Ok(())
 }
 
-fn get_cars(count: u8) -> Result<Vec<CarResponse>, reqwest::Error> {
-    let url: &str = "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png&limit=1";
-
+fn get_cars(count: u8) -> Result<Vec<CarResponse>, curl::Error> {
     let mut cars: Vec<CarResponse> = Vec::new();
     if count > 1 {
         cars.reserve_exact(usize::from(count) - 1);
     }
 
-    for i in 1..=count {
-        println!("fetching {i}th car");
-        let response: String = reqwest::blocking::get(url)?.text()?;
-        let response: Vec<CarResponse> = match serde_json::from_str(&response) {
-            Ok(k) => k,
-            Err(e) => {
-                eprint!("[Error]: {}", e);
-                continue;
-                //std::process::exit(1);
+    let url: &str = "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png&limit=1";
+    let mut handle = Easy::new();
+    handle.url(&url)?;
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            let response: Result<Vec<CarResponse>, serde_json::Error> =
+                serde_json::from_slice(data);
+
+            match response {
+                Ok(car) => {
+                    cars.extend(car);
+                }
+                Err(e) => {
+                    eprintln!("Error occured while parsing data\n{e}");
+                }
             }
-        };
-        cars.push(response[0].clone())
+
+            Ok(data.len())
+        })?;
+
+        for i in 1..=count {
+            println!("fetching {i}th car");
+            transfer.perform()?;
+        }
     }
     Ok(cars)
 }
 
-fn download_cars(cars: &Vec<CarResponse>, save_path: &str) -> Result<(), reqwest::Error> {
+fn download_cars(cars: &Vec<CarResponse>, save_path: &str) -> Result<(), curl::Error> {
     //TODO make it async
     for car in cars {
+        println!("downloading {} from {}", car.id, car.url);
         if let Some(extension) = Path::new(&car.url).extension().and_then(OsStr::to_str) {
-            let img_bytes = reqwest::blocking::get(&car.url)?.bytes()?;
-            let image = match image::load_from_memory(&img_bytes) {
-                Ok(k) => k,
-                Err(e) => {
-                    eprintln!("Error occured while loading image from memory\n{e}");
-                    continue;
-                }
-            };
-            let img_path = format!("{}/{}.{}", save_path, car.id, extension);
-            match image.save(&img_path) {
+            let mut img = Vec::new();
+            let img_path: String = format!("{}/{}.{}", save_path, car.id, extension);
+
+            let mut handle = Easy::new();
+            handle.url(&car.url)?;
+
+            {
+                let mut transfer = handle.transfer();
+                transfer.write_function(|data| {
+                    img.extend_from_slice(data);
+                    Ok(data.len())
+                })?;
+
+                transfer.perform()?;
+            }
+
+            match std::fs::write(&img_path, &img) {
                 Ok(_) => {
-                    println!("car saved to: {img_path}");
+                    println!("car saved to {}", img_path);
                 }
                 Err(e) => {
                     eprintln!("Error occured while saving image\n{e}");
