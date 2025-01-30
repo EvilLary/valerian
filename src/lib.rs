299149d64851885eb8688a42fed329181399ad65
EvilLary <EvilLary@proton.me>
2025-01-30 16:35:32 +0300

    colors and stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,81 @@
 use std::env;
 use std::path::PathBuf;
 
-pub const API_URL: &str =
-    "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png,gif&limit=1";
-pub const HELP_MSG: &str = r#"Small app to fetch cats pic from TheCatAPI
+pub mod colors;
 
-    USAGE:
-        valerian [FLAGS] [OPTIONS]
-
-    [FLAGS]:
-        -h        prints help information
-        -v        prints version information
-        -V        enable verbose output
-
-    [OPTIONS]:
-        -c        number of cats to fetch and download [default: 1]
-        -o        output directory [default: current working directory]"#;
-
+pub struct CarResponse {
+    pub id: String,
+    pub url: String,
+}
 pub struct CmdArgs {
     pub count: u8,
     pub output: PathBuf,
-    pub verbose: bool,
-    //pub version: bool,
     pub help: bool,
 }
 
+pub enum ValResult<T> {
+    Ok(T),
+    Err(ValError),
+}
+
+pub enum ValError {
+    InvalidArgumnet,
+    LitteArgs,
+    ReadOnlyAccess,
+    NotADirectory,
+    InvalidOption(String),
+}
+
 impl CmdArgs {
-    pub fn get() -> Self {
+    pub fn get() -> ValResult<Self> {
         let mut count = 1;
         let mut output = env::current_dir().unwrap();
-        let mut verbose: bool = false;
         let mut help: bool = false;
-        //let mut version: bool = false;
         let mut args = env::args().skip(1);
 
         while let Some(item) = args.next() {
-            if item == "-c" {
-                if let Some(c) = args.next() {
-                    match c.parse::<u8>() {
-                        Ok(k) => count = k,
-                        Err(_e) => {
-                            eprintln!("Invalid value passed to -c");
+            match item.as_str() {
+                "-c" => {
+                    if let Some(c) = args.next() {
+                        match c.parse::<u8>() {
+                            Ok(k) => count = k,
+                            Err(_e) => {
+                                return ValResult::Err(ValError::InvalidArgumnet);
+                            }
                         }
-                    }
-                }
-            } else if item == "-o" {
-                if let Some(o) = args.next() {
-                    let path = PathBuf::from(o);
-
-                    if path.is_dir() {
-                        if path.metadata().unwrap().permissions().readonly() {
-                            eprint!("Don't have write access to {}", path.display());
-                            std::process::exit(1);
-                        }
-                        output = path;
                     } else {
-                        eprint!("{} isn't a directory", path.display());
-                        std::process::exit(1);
+                        return ValResult::Err(ValError::LitteArgs);
                     }
                 }
-            } else if item == "-h" {
-                help = true;
-            } else if item == "-V" {
-                verbose = true;
-            } else {
-                eprintln!("Invalid argument");
+                "-o" => {
+                    if let Some(o) = args.next() {
+                        let path = PathBuf::from(o);
+
+                        if path.is_dir() {
+                            // this stupid function is so unreliable it's insane
+                            //println!("{:?}",path.metadata().unwrap().permissions().readonly());
+                            //
+                            //if !path.metadata().unwrap().permissions().readonly() {
+                            //    println!("fdf");
+                            //    return ValResult::Err(ValError::ReadOnlyAccess);
+                            //}
+                            output = path;
+                        } else {
+                            return ValResult::Err(ValError::NotADirectory);
+                        }
+                    } else {
+                        return ValResult::Err(ValError::LitteArgs);
+                    }
+                }
+                "-h" => help = true,
+                _ => return ValResult::Err(ValError::InvalidOption(item)),
             }
         }
 
-        Self {
+        ValResult::Ok(Self {
             count,
             output,
-            verbose,
             help,
-        }
+        })
     }
 }
-//pub fn parse_args() -> (u8, PathBuf) {
-//    let args = env::args().skip(1).collect::<Vec<String>>();
-//    let count: u8 = match args.first() {
-//        Some(c) => {
-//            if let Ok(n) = c.parse() {
-//                n
-//            } else {
-//                eprint!("First argument has to be a number");
-//                std::process::exit(1);
-//            }
-//        }
-//        None => 1,
-//    };
-//
-//    let save_path: PathBuf = match args.get(1) {
-//        Some(o) => {
-//            let path = PathBuf::from(o);
-//
-//            if path.is_dir() {
-//                if path.metadata().unwrap().permissions().readonly() {
-//                    eprint!("Don't have write access to {}", path.display());
-//                    std::process::exit(1);
-//                }
-//                path
-//            } else {
-//                eprint!("{} isn't a directory", path.display());
-//                std::process::exit(1);
-//            }
-//        }
-//        None => env::current_dir().expect("Failed to retrieve current directory"),
-//    };
-//
-//    (count, save_path)
-//}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,58 @@
 //All credit goes to https://github.com/plastic-bottleneck/ccat
 //This is just for learning rust
 
+use colors::*;
 use curl::easy::Easy;
 use std::{
     io::{self, Write},
     path::PathBuf,
+    process::exit,
 };
 use valerian::*;
 
-struct CarResponse {
-    id: String,
-    url: String,
-}
+
+const API_URL: &str =
+    "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png,gif&limit=1";
 
 fn main() -> Result<(), curl::Error> {
-    //let (count, save_path) = valerian::parse_args();
+    let args = match CmdArgs::get() {
+        ValResult::Ok(k) => k,
+        ValResult::Err(e) => {
+            match e {
+                ValError::NotADirectory => {
+                    eprintln!("Provided directory isn't one or it doesn't exists");
+                }
+                ValError::InvalidArgumnet => {
+                    eprintln!("Invalid argumnet passed in, {BOLD}see valerian -h for help{RESET} ");
+                }
+                ValError::ReadOnlyAccess => {
+                    eprintln!(
+                        "Insuffucient permissions to the provided directory {RED}Read Only{RESET}"
+                    );
+                }
+                ValError::InvalidOption(n) => {
+                    eprintln!(
+                        "Invalid option {:?}\nsee {BOLD}valerian -h{RESET} for options",
+                        n
+                    )
+                }
+                ValError::LitteArgs => {
+                    eprintln!("No argumnet provided to option");
+                }
+            }
+            exit(1);
+        }
+    };
 
-    let args = CmdArgs::get();
     if args.help {
-        println!("{HELP_MSG}");
+        //println!("{HELP_MSG}");
+        help_msg();
         return Ok(());
     }
     let response = get_cars(args.count)?;
 
     if let Err(e) = download_cars(&response, &args.output) {
-        eprintln!("Error occured while download cars: {e}");
+        eprintln!("{RED}Error occured while downloading cars{RESET}: {e}");
         std::process::exit(1);
     }
 
@@ -61,7 +89,7 @@ fn get_cars(count: u8) -> Result<Vec<CarResponse>, curl::Error> {
         })?;
 
         for i in 1..=count {
-            println!("fetching {i}th car");
+            println!("{CYAN}fetching {i}th car{RESET}");
             transfer.perform()?;
         }
     }
@@ -72,14 +100,17 @@ fn download_cars(cars: &[CarResponse], save_path: &PathBuf) -> Result<(), curl::
     //TODO make it async?
     let mut handle = Easy::new();
     for car in cars {
-        println!("downloading {} from: {}", car.id, car.url);
+        println!(
+            "downloading {} from: {BOLD}{BLUE}{}{RESET}",
+            car.id, car.url
+        );
 
         let extension = &car.url[car.url.len() - 3..];
         let img_path: String = format!("{}/{}.{}", save_path.display(), car.id, extension);
         let img_file = match std::fs::File::create_new(&img_path) {
             Ok(k) => k,
             Err(e) => {
-                eprintln!("Error occured while creating img file\n{e}");
+                eprintln!("{RED}Error occured while creating img file{RESET}\n{e}");
                 continue;
             }
         };
@@ -93,7 +124,7 @@ fn download_cars(cars: &[CarResponse], save_path: &PathBuf) -> Result<(), curl::
                 match img_file.write(data) {
                     Ok(_) => {}
                     Err(e) => {
-                        eprintln!("Error occured while saving image\n{e}");
+                        eprintln!("{RED}Error occured while saving image{RESET}\n{e}");
                     }
                 }
                 Ok(data.len())
@@ -102,7 +133,28 @@ fn download_cars(cars: &[CarResponse], save_path: &PathBuf) -> Result<(), curl::
             transfer.perform()?;
         }
         //TODO: This seems kinda stupid? it'll print out this message even if image saving wasn't successful
-        println!("car saved to {}", img_path);
+        println!("{BLUE}car saved to {BOLD}{GREEN}{}{RESET}", img_path);
     }
     Ok(())
+}
+
+fn help_msg() {
+
+    let msg = format!("Small app to fetch cats pic from TheCatAPI
+
+{GREEN}USAGE:{RESET}
+    valerian {CYAN}[FLAGS] [OPTIONS]{RESET}
+
+{GREEN}[FLAGS]:{RESET}
+    {CYAN}-h        prints help information{RESET}
+
+{GREEN}[OPTIONS]:{RESET}
+    {CYAN}-c        number of cats to fetch and download [default: 1]
+    -o        output directory [default: current working directory]{RESET}");
+
+    //std::io::stdout()
+    //    .lock()
+    //    .write_all(format!("{}\n", msg).as_bytes())
+    //    .expect("Failed to write to stdout");
+    println!("{msg}");
 }
